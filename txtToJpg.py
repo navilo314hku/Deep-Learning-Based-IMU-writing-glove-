e24b8a4b86d72e7cae867f47df23de90c1360b81
navilo314 <navilo314@gmail.com>
2022-12-30 18:51:15 +0800

    add resnet training

--- txtToJpg.py
+++ txtToJpg.py
@@ -5,6 +5,62 @@ import numpy as np
 import cv2
 import os 
 from time import sleep
+from threading import *
+class ReadThread(Thread):
+    def arrayFromFile(self,file_name):
+        arr=[]
+        with open(file_name,'r') as f:
+            for line in f.readlines(): #line is a string
+                line_arr=[float(x) for x in line.split(",")]
+                arr.append(line_arr)
+        arr=np.array(arr)
+        return arr
+    def __init__(self,txtFileName):
+        super(ReadThread, self).__init__()
+        self.txtFileName=txtFileName
+
+    def run(self):
+        #return numpy array
+        print("ReadThread.run()")
+        img_array=self.arrayFromFile(self.txtFileName)
+        return img_array
+
+    def start(self):
+        return self.run()
+class WriteThread(Thread):
+    def __init__(self,output_path,img_array):
+        super(WriteThread, self).__init__()
+        self.output_path=output_path
+        self.img_array=img_array
+    def run(self):
+        cv2.imwrite(self.output_path,self.img_array)
+        return 
+
+def threadStoreTxtToJpg(TXT_PATH,IMAGE_PATH,label):
+
+    def isEmptyTxt(file_name):
+        return os.stat(f"{file_name}").st_size == 0
+    def currentTimeInfo():
+        return datetime.now().strftime("%Y%m%d_%H%M%S")
+    #convert all txt files from {TXT_PATH} into jpg images and store them in {IMAGE_PATH}
+    #1. list all files name with txt extension
+    destination_folder= os.path.join(IMAGE_PATH,f"{label}")
+    for file in os.listdir(TXT_PATH):
+        if file.endswith(".txt"):
+            file_name=os.path.join(TXT_PATH,file)
+            if not isEmptyTxt(file_name): 
+                print(file_name)
+                t1=ReadThread(file_name)#init thread object
+                t1.start()
+                t1.join()
+                #img_array=arrayFromFile(file_name)
+                #sleep(0.8)
+               
+                output_path=os.path.join(destination_folder,currentTimeInfo()+".jpg")
+                Writethread=WriteThread(output_path,img_array)
+                Writethread.start()
+                Writethread.join()
+#                cv2.imwrite(output_path,img_array)
 def storeTxtToJpg(TXT_PATH,IMAGE_PATH,label):
     def arrayFromFile(file_name):
         arr=[]
@@ -27,7 +83,7 @@ def storeTxtToJpg(TXT_PATH,IMAGE_PATH,label):
             if not isEmptyTxt(file_name): 
                 print(file_name)
                 img_array=arrayFromFile(file_name)
-                sleep(0.8)
+                sleep(1)
                 #cv2.imwrite(img_)
                 #path=os.path.join("images","your_file.jpg")
                 output_path=os.path.join(destination_folder,currentTimeInfo()+".jpg")
